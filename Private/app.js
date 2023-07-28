401b668b21dc1de43906567d527f481096a04046
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-07-28 10:36:09 +0200

    adding basic server setup

---
+++ Private/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+require('dotenv').config;
+const bodyParser = require ('body-parser')
+
+const nodemailer = require('nodemailer');
+const validator = require('validator');
+
+const port = process.env.PORT || 4000;
+
+app.use(express.static('../Public/'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('contact', (req, res, next) =>{
+    
+})
+
+app.listen(port, () => {
+    console.log('Server is listening on Post 4000');
+})
