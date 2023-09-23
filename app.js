d40c8cbabcdae5eaacc137b390449d2ad6fc4c83
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-09-23 18:28:42 +0200

    Added inital csp in prep for deploy

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ const sendMail = require('./Private/serverJs/sendmail');
 const validator = require('validator');
 const handler = require('./Private/serverJs/handleContact')
 
+app.use(function (req, res, next) {
+    res.setHeader(
+      'Content-Security-Policy',
+        "default-src 'self'; font-src 'self' https://fonts.gstatic.com ; img-src 'self' 'unsafe-inline' ; script-src 'self'; style-src 'self'; frame-src 'self'; connect-src http://localhost:4000 https://theweddingcreche-production.up.railway.app/ ; worker-src blob:; child-src blob:"
+    );
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+  
+  
+    const allowedOrigins = ['https://theweddingcreche-production.up.railway.app/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+  
+  
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
 const port = process.env.PORT || 4000;
 
 app.use(express.static('views'))
